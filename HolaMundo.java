4a2672def38f898abf89c067a149f1c9b567463f
duducito15 <84535246+duducito15@users.noreply.github.com>
2025-05-15 11:56:42 -0500

    Tu chero

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        //Muestra en pantalla la frase Hola Mundo!!!
+        System.out.println("Hola Mundo!!!");
+    }
+}
