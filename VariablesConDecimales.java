7aed649c60dc09d233b9431dd6bd7fa4a0d63d21
duducito15 <84535246+duducito15@users.noreply.github.com>
2025-05-26 09:37:15 -0500

    uso de cadenas y letras

---
+++ VariablesConDecimales.java
@@ -0,0 +1,14 @@
+public class VariablesConDecimales {
+
+    public static void main(String[] args) {
+        // Dos tipos double y float
+        double x;
+        double y;
+
+        x = 7;
+        y = 25.0023;
+
+        System.out.println("x vale " + x);
+        System.out.println("y vale " + y);
+    }
+}
