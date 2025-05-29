4eb2f242639a44eeb10771efe7b385efe1111ee6
duducito15 <84535246+duducito15@users.noreply.github.com>
2025-05-29 10:29:09 -0500

    operadores

--- UsoDeChar.java
+++ UsoDeChar.java
@@ -8,5 +8,6 @@ public class UsoDeChar {
         System.out.println("letra 1: " + letra1);
         System.out.println("letra 3: " + letra3);
         System.out.println("Todas juntas: " + letra1 + letra2 + letra3 + letra4);
+        System.out.println(letra1 +""+ letra2 +""+ letra3);
     }
 }
---
+++ UsoDeOperadoresAritmeticos.java
@@ -0,0 +1,10 @@
+public class UsoDeOperadoresAritmeticos {
+    public static void main(String[] args) {
+        int numero = 100;
+        System.out.println(numero + " " + (numero + 5) + " " + (numero - 5));
+        System.out.println(numero + " " + (numero * 5) + " " + (numero / 5));
+        System.out.println(numero + " " + (numero % 5));
+        // System.out.println(x++);
+        // System.out.println(x--);
+    }
+}
