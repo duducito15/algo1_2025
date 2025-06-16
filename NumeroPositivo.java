f150bd729ada1e3d2da13281e6379abb9fef434b
duducito15 <84535246+duducito15@users.noreply.github.com>
2025-06-16 08:59:38 -0500

    operadores de comparacion

---
+++ NumeroPositivo.java
@@ -0,0 +1,24 @@
+public class NumeroPositivo {
+    public static void main(String[] args) {
+        int numero;
+
+        System.out.println("Por favor, ingresa un número entero");
+        numero = Integer.parseInt(System.console().readLine());
+
+        if (numero > 0) {
+            System.out.println("El número ingresado es Positivo");
+        } else {
+            System.out.println("El número ingresado es Negativo");
+        }
+
+        /* Operadores de comparación
+        
+         * ==   igual
+         * !=   distinto 
+         * <    menor que
+         * >    mayor que
+         * <=   menor o igual que
+         * >=   mayor o igual que
+         */
+    }
+}
