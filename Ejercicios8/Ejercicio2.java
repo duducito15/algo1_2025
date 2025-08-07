836c3caa475b135eacd76a67f90e30f7dbacdf94
duducito15 <84535246+duducito15@users.noreply.github.com>
2025-08-07 11:25:25 -0500

    Ejercicio 2 - Array bi completo

--- Ejercicios8/Ejercicio2.java
+++ Ejercicios8/Ejercicio2.java
@@ -6,20 +6,38 @@ public class Ejercicio2 {
 
         int f, c;
 
-        for ( f = 0; f < 4; f++) {
-            for ( c = 0; c < 5; c++) {
-                num[f][c] = (int) (Math.random()*101);
+        for (f = 0; f < 4; f++) {
+            for (c = 0; c < 5; c++) {
+                num[f][c] = (int) (Math.random() * 101);
             }
         }
 
         int sumaFila;
-        for ( f = 0; f < 4; f++) {
+        for (f = 0; f < 4; f++) {
             sumaFila = 0;
-            for ( c = 0; c < 5; c++) {
+            for (c = 0; c < 5; c++) {
                 System.out.printf("%7d   ", num[f][c]);
                 sumaFila = sumaFila + num[f][c];
             }
             System.out.printf("| %7d\n", sumaFila);
         }
+
+        for (c = 0; c < 5; c++) {
+            System.out.print("---------");
+        }
+        System.out.println("---------");
+
+        int sumaColumna;
+        int sumaTotal = 0;
+
+        for (c = 0; c < 5; c++) {
+            sumaColumna = 0;
+            for (f = 0; f < 4; f++) {
+                sumaColumna = sumaColumna + num[f][c];
+            }
+            sumaTotal = sumaTotal + sumaColumna;
+            System.out.printf("%7d   ", sumaColumna);
+        }
+        System.out.printf("| %7d\n", sumaTotal);
     }
 }
